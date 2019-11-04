611dc17d3e33eda0befcdc50c9ca9a955bf1e2f4
Colin Daniels <colin.r.daniels@gmail.com>
2019-11-04 12:10:55 -0500

    Simple function to load and smooth the dataset
    Takes quite a while to run though.

--- scripts/io.py
+++ scripts/io.py
@@ -2,6 +2,8 @@ import pickle
 import numpy as np
 import typing as tp
 
+from scripts.smoothing import smooth_raman
+
 
 class RawRaman:
     def __init__(
@@ -26,3 +28,29 @@ def load_raman_pkl(path: str) -> tp.Generator[RawRaman, None, None]:
                 yield RawRaman(**pickle.load(f))
             except EOFError:
                 break
+
+
+def load_smoothed_dataset(
+        path: str,
+        min_freq: float,
+        max_freq: float,
+        points: int,
+        width: float
+):
+    X = []
+    y = []
+
+    for entry in load_raman_pkl(path):
+        print(entry.name)
+        smoothed = smooth_raman(
+            min_freq=min_freq,
+            max_freq=max_freq,
+            points=points,
+            width=width,
+            frequency=entry.frequency,
+            intensity=entry.intensity,
+        )
+        X.append(smoothed)
+        y.append([entry.width, entry.length])
+
+    return np.array(X), np.array(y)
