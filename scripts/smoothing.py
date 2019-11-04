38f60b56470b13db706470b122ea24a5885047c8
Colin Daniels <colin.r.daniels@gmail.com>
2019-11-04 11:49:07 -0500

    Extract smoothing function

--- scripts/smoothing.py
+++ scripts/smoothing.py
@@ -14,13 +14,30 @@ def smooth_raman_json(min_freq, max_freq, points, width, filename):
             frequency.append(file['frequency'][i])
             intensity.append(file['average-3d'][i])
 
+    return smooth_raman(
+        min_freq,
+        max_freq,
+        points,
+        width,
+        frequency=np.array(frequency),
+        intensity=np.array(intensity),
+    )
+
+
+def smooth_raman(min_freq, max_freq, points, width, frequency, intensity):
+    # Only select peaks within the given frequency range
+    peaks_in_range = (min_freq <= frequency) & (frequency <= max_freq)
+    intensity = intensity[peaks_in_range]
+    frequency = frequency[peaks_in_range]
+
     # Lorentzian distribution smoothing for the data set
     frequency_smooth = np.linspace(min_freq, max_freq, points)
     intensity_smooth = np.zeros(points)
-    for i, fi in enumerate(frequency_smooth):
-        for j, fj in enumerate(frequency):
-            height = intensity[j]
-            intensity_smooth[i] += height * (0.5 * width)**2 / (
-                        (fi - fj)**2 + (0.5 * width)**2)
+
+    gamma_sq = (0.5 * width)**2
+    for i, f_eval in enumerate(frequency_smooth):
+        intensity_smooth[i] = np.sum(
+            intensity * gamma_sq / ((f_eval - frequency)**2 + gamma_sq)
+        )
 
     return intensity_smooth
